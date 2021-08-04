79e42853ae23fcb46ac9e565d553a26de3f3c256
vijaya1212 <ajnabee_333@yahoo.com>
2021-08-04 16:05:00 +0530

    first commit from eclipse

---
+++ secondprojectonmaven/src/test/java/secondprojectonmaven/secondtest.java
@@ -0,0 +1,11 @@
+package secondprojectonmaven;
+
+import org.testng.annotations.Test;
+
+public class secondtest {
+
+	@Test
+	public void test() {
+		System.out.println("...second test for project on maven");
+	}
+}
